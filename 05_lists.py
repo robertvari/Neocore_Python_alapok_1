9fd1d59cee8330f1fc89dbcaf07c7c6453476d94
Robert Var <mail.robertvari@gmail.com>
2022-09-05 16:00:18 +0200

    Update

---
+++ 05_lists.py
@@ -0,0 +1,11 @@
+#                0          1       2         3          4
+my_friends = ["Robert", "Csaba", "Norbi", "Kriszta", "Csilla"]
+#                 -5       -4       -3        -2        -1
+
+print(my_friends)
+print(len(my_friends))
+
+print(my_friends[3])
+print(my_friends[-1])
+
+print(my_friends[ len(my_friends)//2 ])
