356235c12f6d632e168284123129916cc7aa4513
Robert Var <mail.robertvari@gmail.com>
2022-09-05 15:16:48 +0200

    Init repo

---
+++ 01_datatypes.py
@@ -0,0 +1,28 @@
+1  # int
+3.14  # float
+
+# string
+"Hello World"
+'Hello World'
+
+"It's my birthday"
+
+# list
+[1, 3.14, "Hello World"]
+
+# tuple
+(1, 3.14, "Hello World")
+
+# set
+{1, 3.14, "Hello World"}
+
+# dictionary
+{
+    "name": "Robert",
+    "address": "Budapest",
+    "email": "robert@gmail.com"
+}
+
+# booleans
+True
+False
