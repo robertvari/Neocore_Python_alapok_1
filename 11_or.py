a9e6350a2f2432f91d0260c41de3477579e8efdb
Robert Var <mail.robertvari@gmail.com>
2022-09-05 17:31:46 +0200

    Update

--- 11_or.py
+++ 11_or.py
@@ -1,5 +1,6 @@
 number = 11
 
-#      True            False
-if number == 10 or number == 20:
+#             True                                 True
+#          True         False              False            True
+if (number == 10 or number == 20) and (number == 100 or number == 200):
     print("The number is 10 or 20")
