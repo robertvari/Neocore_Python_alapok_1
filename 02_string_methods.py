fa6dc283d5e0f03a4d89c0fc83aeecd1e09f6be1
Robert Var <mail.robertvari@gmail.com>
2022-09-05 15:30:41 +0200

    Update

---
+++ 02_string_methods.py
@@ -0,0 +1,4 @@
+print("Hello World. My name is Robert".upper())
+print("Hello World. My name is Robert".lower())
+print("Hello World. My name is Robert".replace("Robert", "Csaba"))
+print("Hello World. My name is Robert".split("My"))
