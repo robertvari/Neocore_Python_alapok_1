bc37d2866e39f426c8cfe7b22e842de8db3d9757
Robert Var <mail.robertvari@gmail.com>
2022-09-05 15:40:24 +0200

    Update

---
+++ 03_variables.py
@@ -0,0 +1,11 @@
+my_name = "Csaba"  # declare/define a variable
+
+my_name = "Robert"  # override the variable
+
+print(my_name)  # reference a variable
+
+name, age, address = "Robert", 18, "Budapest"
+
+print(name)
+print(age)
+print(address)
